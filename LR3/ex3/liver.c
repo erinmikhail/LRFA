f44d14ee1a7fa38a7d46801a6ce791f085ba425b
Misha Erin <michael.er1n@icloud.com>
2025-11-19 11:32:18 +0300

    добавлена функця сравнения возраста в ливере.с

--- LR3/ex3/liver.c
+++ LR3/ex3/liver.c
@@ -10,3 +10,15 @@ void print_liver(const Liver *liver){
            liver->birth_date.tm_year + 1900, liver->gender, liver->income);
 }
 
+int compare_liver_age(const Liver *l1, const Liver *l2){
+    if(l1->birth_date.tm_year != l2->birth_date.tm_year){
+        return l1->birth_date.tm_year - l2->birth_date.tm_year;
+    }
+    if (l1->birth_date.tm_mon != l2->birth_date.tm_mon){
+        l1->birth_date.tm_mon - l2->birth_date.tm_mon;
+    }
+    if (l1->birth_date.tm_mday != l2->birth_date.tm_mday){
+        return l1->birth_date.tm_mday - l2->birth_date.tm_mday;
+    }
+}
+
