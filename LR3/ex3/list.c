aeb86d11109f7b5ee883a04c98937d7f64edbb78
Misha Erin <michael.er1n@icloud.com>
2025-11-19 13:37:06 +0300

    написана инициализация списка и его очистка полностью

--- LR3/ex3/list.c
+++ LR3/ex3/list.c
@@ -1,1 +1,18 @@
 #include "list.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+LinkedList create_list(void){
+    LinkedList list;
+    list.head = NULL;
+    list.tail = NULL;
+    list.size = 0;
+    return list;
+}
+
+void erase_list(LinkedList *list){
+    while (list->size > 0){
+        pop_frint_list(list);
+    }
+}
