0007212c8f8896e76c516ee9f70976e0d4e6d4b4
Misha Erin <michael.er1n@icloud.com>
2025-11-26 15:41:27 +0300

    сделан заголовочный файл для почтовоц системы

---
+++ LR3/ex4_5/include/post_system.h
@@ -0,0 +1,68 @@
+#ifndef POST_SYSTEM_H
+#define POST_SYSTEM_H
+
+#include "heap.h"
+#include <time.h>
+
+typedef enum{
+    IN_TRANSIT,
+    DELIVERED,
+    UNDELIVERED
+} LetterState;
+
+typedef enum{
+    REGULAR,
+    URGENT
+} LetterType;
+
+typedef struct{
+    int id;
+    LetterType type;
+    LetterState state;
+    int priority;
+    int from_post_id;
+    int to_post_id;
+    char tech_data[100];
+    int current_post_id;
+}Letter;
+
+typedef struct{
+    int id;
+    int max_capacity;
+    Heap *letter_queue;
+    int *connected_ids;
+    int connected_count;
+    int current_load;
+} PostOffice;
+
+typedef struct{
+    PostOffice **offices;
+    Letter **letter;
+    int office_count;
+    int letter_count;
+    int offices_capacity;
+    int letters_capacity;
+    int next_letter_id;
+    char *log_filename;
+}PostSystem;
+
+PostSystem* create_post_system(const char *log_filename);
+void delete_post_system(PostSystem *system);
+
+int add_post_office(PostSystem *system, int id, int max_capacity, const int *connected_ids, int connected_count);
+int remove_post_office(PostSystem *system, int id);
+
+int add_letter(PostSystem *system, LetterType type, int priority, int from_post_id, int to_post_id, const char *tech_data);
+int mark_letter_undeliverable(PostSystem *system, int letter_id);
+int pickup_letter(PostSystem *system, int letter_id);
+
+void process_mail_delivery(PostSystem *system);
+int find_next_office(const PostSystem *system, int current_office_id, int target_office_id);
+
+PostOffice* find_office_by_id(const PostSystem *system, int id);
+Letter* find_letter_by_id(const PostSystem *system, int id);
+void log_event(const PostSystem *system, const char *message);
+
+void print_all_letters(const PostSystem *system, const char *filename);
+int load_mappings_from_file(PostSystem *system, const char *filename);
+#endif
