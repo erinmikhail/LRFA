d0ceea73e369d610f11cc0e733d9d7f0f6bd6643
Misha Erin <michael.er1n@icloud.com>
2025-11-26 17:57:34 +0300

    init creat_post_system

--- LR3/ex4_5/src/post_system.c
+++ LR3/ex4_5/src/post_system.c
@@ -4,3 +4,39 @@
 #include <time.h>
 #include "../include/post_system.h"
 
+PostSystem* create_post_system(const char *log_filename){
+    PostSystem *system = malloc(sizeof(PostSystem));
+    system->offices_capacity = 10;
+    system->letters_capacity = 30;
+    system->office_count = 0;
+    system->letter_count = 0;
+    system->next_letter_id = 1;
+    strncpy(system->log_filename, log_filename, sizeof(system->log_filename) - 1);
+    system->log_filename[sizeof(system->log_filename) - 1] = '\0';
+
+    system->offices = malloc(system->offices_capacity * sizeof(PostOffice));
+    system->letters = malloc(system->letters_capacity * sizeof(Letter));
+
+    if (!system->offices || !system->letters){
+        free(system->offices);
+        free(system->letters);
+        free(system);
+        return NULL;
+    }
+
+    FILE *log_file = fopen(log_filename, "w");
+    if (log_file){
+        fclose(log_file);
+    }
+
+    log_event(system, "Post system init\n");
+    return system;
+}
+
+void delete_post_system(PostSystem *system){
+    if (!system){
+        return;
+    }
+
+    log_event(system)
+}
