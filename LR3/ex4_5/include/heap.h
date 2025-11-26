ef55b524f968afb5a86f203447e810dbf816362e
Misha Erin <michael.er1n@icloud.com>
2025-11-26 17:16:13 +0300

    исправил разногласия между h и с

--- LR3/ex4_5/include/heap.h
+++ LR3/ex4_5/include/heap.h
@@ -14,7 +14,7 @@ void delete_heap(Heap *h);
 int is_empty_heap(const Heap *h);
 size_t size_heap(const Heap *h);
 int peek_heap(const Heap *h);
-void push_heap(Heap *h, int value);
+int push_heap(Heap *h, int value);
 int pop_heap(Heap *h);
 Heap build_heap(const int *array, size_t n);
 int is_equal_heap(const Heap *h1, const Heap *h2);
