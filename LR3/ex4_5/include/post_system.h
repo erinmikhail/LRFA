5e96a28add8a7b47025886983ae55eea46b7cddf
Misha Erin <michael.er1n@icloud.com>
2025-11-26 17:41:09 +0300

    решил убрать лишнее

--- LR3/ex4_5/include/post_system.h
+++ LR3/ex4_5/include/post_system.h
@@ -29,21 +29,21 @@ typedef struct{
 typedef struct{
     int id;
     int max_capacity;
-    Heap *letter_queue;
+    Heap letter_queue;
     int *connected_ids;
     int connected_count;
     int current_load;
 } PostOffice;
 
 typedef struct{
-    PostOffice **offices;
-    Letter **letter;
+    PostOffice *offices;
+    Letter *letters;
     int office_count;
     int letter_count;
     int offices_capacity;
     int letters_capacity;
     int next_letter_id;
-    char *log_filename;
+    char log_filename[100];
 }PostSystem;
 
 PostSystem* create_post_system(const char *log_filename);
@@ -57,12 +57,10 @@ int mark_letter_undeliverable(PostSystem *system, int letter_id);
 int pickup_letter(PostSystem *system, int letter_id);
 
 void process_mail_delivery(PostSystem *system);
-int find_next_office(const PostSystem *system, int current_office_id, int target_office_id);
 
 PostOffice* find_office_by_id(const PostSystem *system, int id);
 Letter* find_letter_by_id(const PostSystem *system, int id);
 void log_event(const PostSystem *system, const char *message);
 
 void print_all_letters(const PostSystem *system, const char *filename);
-int load_mappings_from_file(PostSystem *system, const char *filename);
 #endif
