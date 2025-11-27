cfa54878a30a638949d33c543f956285cd7edd05
Misha Erin <michael.er1n@icloud.com>
2025-11-27 21:56:45 +0300

    написано пару функ..

--- LR3/ex7/src/interpreter.c
+++ LR3/ex7/src/interpreter.c
@@ -41,3 +41,42 @@ void init_interpreter(){
 void cleanup_interpreter(){
     //заглушка
 }
+
+int get_variable_value(char name){
+    if (name < 'A' || name > 'Z'){
+        return 0;
+    }
+    return variables[name - 'A'];
+}
+
+void get_variable_value(char name, int value){
+    if (name >= 'A' && name <= 'Z'){
+        variables[name - 'A'] = value;
+        initialized[name - 'A'] = 1;
+    }
+}
+
+void get_variables_state(char* buffer, int buffer_size){
+    buffer[0] = '\0';
+    int first = 1;
+
+    for (int i = 0; i < NUM_VARIABLES; i++){
+         if (initialized[i]) {
+            char var_str[16];
+            if (first) {
+                snprintf(var_str, sizeof(var_str), "%c=%d", 'A' + i, variables[i]);
+                first = 0;
+            } else {
+                snprintf(var_str, sizeof(var_str), ", %c=%d", 'A' + i, variables[i]);
+            }
+            
+            if (strlen(buffer) + strlen(var_str) < buffer_size) {
+                strcat(buffer, var_str);
+            }
+        }
+    }
+
+        if (buffer[0] == '\0') {
+        strcpy(buffer, "No variables");
+    }
+}
