ad5b255a69122b81182a4327566f51d74cb74f05
Misha Erin <michael.er1n@icloud.com>
2025-11-27 21:43:54 +0300

    написано пару стурктур и заглушка

--- LR3/ex7/src/interpreter.c
+++ LR3/ex7/src/interpreter.c
@@ -5,4 +5,39 @@
 
 #include "../include/interpreter.h"
 
-static int 
+static int variables[NUM_VARIABLES];
+static int initialized[NUM_VARIABLES];
+
+typedef enum{
+    TOKEN_NUMBER,
+    TOKEN_VARIABLE,
+    TOKEN_OPERATOR,
+    TOKEN_PRINT,
+    TOKEN_ASSIGN,
+    TOKEN_EOF,
+    TOKEN_ERROR
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    int value;
+    char op;
+    char variable;
+} Token;
+
+typedef struct {
+    Token current_token;
+    const char* input;
+    int position;
+} Parser;
+
+void init_interpreter(){
+    for (int i = 0; i < NUM_VARIABLES; i++){
+        variables[i];
+        initialized[i] = 0;
+    }
+}
+
+void cleanup_interpreter(){
+    //заглушка
+}
